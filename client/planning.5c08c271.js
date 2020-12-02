b74c0062f5e87963f9ef400e1275624f6dc4b3e0
chilot <https://github.com/Raigyo>
2020-12-02 19:09:11 +0100

    Updates

---
+++ client/planning.5c08c271.js
@@ -0,0 +1,1 @@
+import{S as a,i as s,s as n,e as t,t as e,c as r,a as c,b as l,d as i,g as o,h as u,n as d}from"./client.fec3da14.js";function f(a){let s,n;return{c(){s=t("h3"),n=e("Planning")},l(a){s=r(a,"H3",{});var t=c(s);n=l(t,"Planning"),t.forEach(i)},m(a,t){o(a,s,t),u(s,n)},p:d,i:d,o:d,d(a){a&&i(s)}}}export default class extends a{constructor(a){super(),s(this,a,null,f,n,{})}}
